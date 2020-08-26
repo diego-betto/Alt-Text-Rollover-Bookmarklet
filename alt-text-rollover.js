30e5745202feae323b26d5d2dde2ad274f541951
codepo8 <chris.heilmann@gmail.com>
2020-08-26 22:52:10 +0200

    added info, fixed rollover on right-aligned images

--- alt-text-rollover.js
+++ alt-text-rollover.js
@@ -1,25 +1,39 @@
-let altDisplay = document.createElement('div');
-document.body.appendChild(altDisplay);
-altDisplay.style.position = 'absolute';
-altDisplay.style.background = 'yellow';
-altDisplay.style.padding = '10px';
-altDisplay.style.fontFamily = 'Sans-serif';
-altDisplay.style.maxWidth = '150px';
-altDisplay.style.top = '-100px';
-altDisplay.style.color = 'black';
-let allimgs = document.querySelectorAll('img');
-let out = '';
-allimgs.forEach(i => {
-  i.addEventListener('mouseover',e => {
-    e.target.style.opacity = 0.7;
-    let coords = e.target.getBoundingClientRect();
-    altDisplay.style.top = coords.top + 5 + 'px';
-    altDisplay.style.left = coords.left + coords.width + 10 + 'px';
-    altDisplay.innerHTML = e.target.alt
+(function(){
+  let altDisplay = document.createElement('div');
+  document.body.appendChild(altDisplay);
+  altDisplay.style.position = 'absolute';
+  altDisplay.style.background = 'yellow';
+  altDisplay.style.padding = '10px';
+  altDisplay.style.fontFamily = 'Sans-serif';
+  altDisplay.style.maxWidth = '150px';
+  altDisplay.style.overflow = 'scroll';
+  altDisplay.style.top = '-100px';
+  altDisplay.style.color = 'black';
+  altDisplay.style.pointerEvents = 'none';
+  let allimgs = document.querySelectorAll('img');
+  let out = '';
+  allimgs.forEach(i => {
+    i.addEventListener('mouseover',e => {
+      e.target.style.opacity = 0.7;
+      let coords = e.target.getBoundingClientRect();
+      altDisplay.style.top = coords.top + 5 + 'px';
+      if(coords.left+coords.width+10+150 > window.innerWidth) {
+        altDisplay.style.left = coords.left - 140 + 'px';
+      } else {
+        altDisplay.style.left = coords.left + coords.width - 30 + 'px';
+      }
+      if(e.target.alt === '') {
+        altDisplay.style.border = '1px solid #c00';
+        altDisplay.innerHTML = 'No alt text!';
+      } else {
+        altDisplay.style.border = '1px solid yellow';
+        altDisplay.innerHTML = e.target.alt
+      }
+    });
+    i.addEventListener('mouseout',e => {
+      e.target.style.opacity = 1;
+      altDisplay.style.left = '-100vw';
+      altDisplay.innerHTML = ''
+    });
   });
-  i.addEventListener('mouseout',e => {
-    e.target.style.opacity = 1;
-    altDisplay.style.left = '-100vw';
-    altDisplay.innerHTML = ''
-  });
-});
+})();
