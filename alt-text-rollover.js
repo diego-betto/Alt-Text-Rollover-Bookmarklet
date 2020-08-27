ffc18824198dc6458e6b64b3ac532c8400fc874d
codepo8 <chris.heilmann@gmail.com>
2020-08-27 13:39:22 +0200

    fixed to screen

--- alt-text-rollover.js
+++ alt-text-rollover.js
@@ -1,30 +1,21 @@
 (function(){
   let altDisplay = document.createElement('div');
   document.body.appendChild(altDisplay);
-  altDisplay.style.position = 'absolute';
+  altDisplay.style.position = 'fixed';
   altDisplay.style.background = 'yellow';
   altDisplay.style.padding = '10px';
   altDisplay.style.fontFamily = 'Sans-serif';
   altDisplay.style.maxWidth = '150px';
   altDisplay.style.overflow = 'scroll';
-  altDisplay.style.top = '-100px';
+  altDisplay.style.top = '10px';
+  altDisplay.style.left = '10px';
   altDisplay.style.fontSize = '14px';
   altDisplay.style.color = 'black';
-  altDisplay.style.pointerEvents = 'none';
   let allimgs = document.querySelectorAll('img');
   allimgs.forEach(i => {
     i.addEventListener('mouseover',e => {
       e.target.style.opacity = 0.7;
-      let coords = e.target.getBoundingClientRect();
-      let top = mouse.y + window.scrollY + 5 + 'px';
-      let left = coords.x + 5 + 'px';
-
-      if(coords.left+20+150 > window.innerWidth) {
-        left = coords.x - 140 + 'px';
-      }
-      altDisplay.style.top = top;
-      altDisplay.style.left = left;
-
+      altDisplay.style.left = '10px';
       if(e.target.alt === '') {
         altDisplay.style.border = '1px solid #c00';
         altDisplay.innerHTML = 'No alt text!';
