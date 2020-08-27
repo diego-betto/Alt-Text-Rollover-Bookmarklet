fb56535b919f6cc1fe38b5491b17b3ce48ab9dfd
codepo8 <chris.heilmann@gmail.com>
2020-08-27 15:27:48 +0200

    Added drag and dropshadow

--- alt-text-rollover.js
+++ alt-text-rollover.js
@@ -11,12 +11,44 @@
   altDisplay.style.left = '10px';
   altDisplay.style.fontSize = '16px';
   altDisplay.style.color = 'black';
+  altDisplay.innerHTML = "Roll over any image<br>Drag to where you want me";
+  altDisplay.style.boxShadow = '3px 3px 20px #333'
+
+  altDisplay.onmousedown = initiatedrag;
+
+  let swatchx = 0;
+  let swatchy = 0;
+  let mousex = 0;
+  let mousey = 0;
+
+  function initiatedrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    mousex = e.clientX;
+    mousey = e.clientY;
+    document.onmouseup = enddrag;
+    document.onmousemove = startdrag;
+  }
+  function startdrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    swatchx = mousex - e.clientX;
+    swatchy = mousey - e.clientY;
+    mousex = e.clientX;
+    mousey = e.clientY;
+    altDisplay.style.top = (altDisplay.offsetTop - swatchy) + "px";
+    altDisplay.style.left = (altDisplay.offsetLeft - swatchx) + "px";
+  }
+  function enddrag() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+
   let allimgs = document.querySelectorAll('img');
   allimgs.forEach(i => {
     i.addEventListener('mouseover',e => {
       e.target.style.opacity = 0.7;
-      altDisplay.style.left = '10px';
-      let out = 'Image: ' + e.target.src +'<br><br>';
+      let out = 'Image: ' + e.target.src.replace(/\/([^\/])/g,'/ $1') +'<br><br>';
       if(e.target.getAttribute('alt') === null) {
         altDisplay.style.border = '1px solid #c00';
         out += 'No alt attribute!';
@@ -27,7 +59,7 @@
         }
         if (e.target.alt !== '') {
           altDisplay.style.border = '1px solid yellow';
-          out += e.target.alt
+          out += `"${e.target.alt}"`;
         }
       }
       altDisplay.innerHTML = out;
